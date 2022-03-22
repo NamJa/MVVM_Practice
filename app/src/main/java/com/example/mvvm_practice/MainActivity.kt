d736d5f1fd067a863e17e2a2e2a6cf809d72d850
Jongwoo Kim <kjwoo810@gmail.com>
2022-03-22 23:12:34 +0900

    applying Basic DataBinding

--- app/src/main/java/com/example/mvvm_practice/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_practice/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.mvvm_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.mvvm_practice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.recyclerView.apply {
+            layoutManager = GridLayoutManager(context, 3)
+        }
     }
 }
