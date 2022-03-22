d2a42035cd0d5b587a4d53beca905f43a6f07bb4
Jongwoo Kim <kjwoo810@gmail.com>
2022-03-23 01:43:11 +0900

    add Sound Adapter with DataBinding

--- app/src/main/java/com/example/mvvm_practice/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_practice/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.mvvm_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.mvvm_practice.databinding.ActivityMainBinding
+import com.example.mvvm_practice.databinding.ListItemSoundBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +16,26 @@ class MainActivity : AppCompatActivity() {
 
         binding.recyclerView.apply {
             layoutManager = GridLayoutManager(context, 3)
+            adapter = SoundAdapter()
+        }
+    }
+
+    private inner class SoundHolder(private val binding: ListItemSoundBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    private inner class SoundAdapter(): RecyclerView.Adapter<SoundHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SoundHolder {
+            val binding = DataBindingUtil.inflate<ListItemSoundBinding>(layoutInflater, R.layout.list_item_sound, parent, false)
+
+            return SoundHolder(binding)
+        }
+
+        override fun onBindViewHolder(holder: SoundHolder, position: Int) {
+        }
+
+        override fun getItemCount(): Int {
+            return 0
         }
     }
 }
