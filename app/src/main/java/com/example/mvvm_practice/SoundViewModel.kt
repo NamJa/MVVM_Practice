14e4369cdc4af6e6972b4cefc3713e078f4974f1
Jongwoo Kim <kjwoo810@gmail.com>
2022-03-23 18:59:01 +0900

    Add Observable data

--- app/src/main/java/com/example/mvvm_practice/SoundViewModel.kt
+++ app/src/main/java/com/example/mvvm_practice/SoundViewModel.kt
@@ -1,11 +1,17 @@
 package com.example.mvvm_practice
 
-class SoundViewModel {
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+
+class SoundViewModel : BaseObservable() {
     var sound: Sound? = null
         set(sound) {
             field = sound
+            notifyChange()
         }
 
+    // SoundViewModel의 RecyclerView에서 바인딩되는 속성에 에노테이션 지정
+    @get:Bindable
     val title: String?
         get() = sound?.name
 }
